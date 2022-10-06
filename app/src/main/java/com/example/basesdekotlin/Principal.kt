b3babbc59b3bdc2cc65785322af4c9f3bea41aa8
AngelSalazarRochaITNL <l18100230@nlaredo.tecnm.mx>
2022-10-06 07:41:21 -0500

    Primeros pasos con Kotlin

---
+++ app/src/main/java/com/example/basesdekotlin/Principal.kt
@@ -0,0 +1,15 @@
+package com.example.basesdekotlin
+//click derecho en principal.kt y despues run principalKt
+fun main()
+{
+    newTopic("Primeros pasos con Kotlin")
+}
+
+fun newTopic(topic: String) //metodos sin tipos son procedimientos
+{
+    /*print("==================== ")
+    print(newTopic("Primeros pasos con Kotlin"))
+    println("==================== $topic ==================== ")
+    print(topic)*/
+    println("========== $topic ========== ")
+}
